50fe46b121c12877667348410f84a6a769f1ee6c
Tooling <nrdc@example.com>
2023-01-02 17:06:35 +0330

    intial project

---
+++ src/main/java/com/alian/learn/AlianApplication.java
@@ -0,0 +1,13 @@
+package com.alian.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alian/learn/AlianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alian.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
